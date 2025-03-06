23c678f4220620f747a3779be1adda83effe85ce
beulahsortie <116152305+beulahsortie@users.noreply.github.com>
2025-03-06 10:38:23 +0800

    Add files via upload
    direct file uploads

---
+++ routes/messages.js
@@ -0,0 +1,54 @@
+const router = require("express").Router();
+const Message = require("../models/Message");
+
+//add
+
+router.post("/", async (req, res) => {
+  const newMessage = new Message(req.body);
+
+  try {
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//get
+
+router.get("/:conversationId", async (req, res) => {
+  try {
+    const messages = await Message.find({
+      conversationId: req.params.conversationId,
+    });
+    res.status(200).json(messages);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
+//testing
+// router.post("/sampp", async (req, res) => {
+//   const newMessage = new Message(req.body);
+
+//   try {
+//     const savedMessage = await newMessage.save();
+//     res.status(200).json(savedMessage);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+// router.get("/:tionId", async (req, res) => {
+//   try {
+//     const messages = await Message.find({
+//       conversationId: req.params.tionId,
+//     });
+//     res.status(200).json(messages);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+module.exports = router;
